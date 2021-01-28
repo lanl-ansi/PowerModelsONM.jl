7f3d6f0e6ded43ddc1559fa151443c415ccae53f
David M Fobes <dfobes@lanl.gov>
2021-01-28 08:55:58 -0700

    ADD: Outage events API specification (#3)
    * ADD: Outage events API specification
    
    Adds a new API model specification for inputs/outputs of
    contingency type events.
    
    The following are two initial examples of the usage of this format:
    
    ```json
    [
      {
        "timestep": "1.0",
        "event_type": "switch",
        "affected_asset": "line.asset_name",
        "event_data": {
          "duration_ms": 100,
          "type": "breaker",
          "state": "open",
          "dispatchable": true,
          "pre_event_actions": [],
          "post_event_actions": [
            {
              "timestep": "1.0",
              "event_type": "loadshed",
              "affected_asset": "load.asset_name",
              "event_data": {
                "status": 0
              }
            }
          ]
        }
      }
    ]
    ```
    
    ```json
    [
      {
        "timestep": "1.0",
        "event_type": "fault",
        "affected_asset": "line.asset_name",
        "event_data": {
          "duration_ms": 100
        }
      }
    ]
    ```
    
    * ADD: events parser / applier

--- src/core/io.jl
+++ src/core/io.jl
@@ -1,5 +1,5 @@
 ""
-function prepare_network_case(network_file::String)::Tuple{Dict{String,Any},Dict{String,Any}}
+function prepare_network_case(network_file::String; events::Vector{Dict}=Vector{Dict}([]))::Tuple{Dict{String,Any},Dict{String,Any}}
     data_dss = PMD.parse_dss(network_file)
 
     # TODO: explicitly support DELTA connected generators in LPUBFDiag
@@ -13,9 +13,48 @@ function prepare_network_case(network_file::String)::Tuple{Dict{String,Any},Dict
 
     data_eng = PMD.parse_opendss(data_dss)
 
+    apply_events!(data_eng, events)
+
     PMD.apply_voltage_bounds!(data_eng)
 
     data_math = PMD.transform_data_model(data_eng; build_multinetwork=true)
 
     return data_eng, data_math
+end
+
+
+""
+function parse_events(events_file::String)::Vector{Dict{String,Any}}
+    open(events_file, "r") do f
+        JSON.parse(f)
+    end
+end
+
+
+""
+function apply_events!(network::Dict{String,Any}, events::Vector{Dict})
+    for event in events
+        source_id = event["affected_asset"]
+        asset_type, asset_name = split(source_id, ".")
+        timestep = event["timestep"]
+
+        if event["event_type"] == "switch"
+            start_timestep = Int(round(parse(Float64, timestep)))
+            if haskey(network, "nw")
+                for (n, nw) in network["nw"]
+                    if parse(Int, n) >= start_timestep
+                        nw["switch"][asset_name]["state"] = Dict{String,PMD.SwitchState}("closed"=>PMD.CLOSED, "open"=>PMD.OPEN)
+                        if haskey(event["event_data"], "dispatchable")
+                            nw["switch"][asset_name]["dispatchable"] = event["event_data"]["dispatchable"] ? PMD.YES : PMD.NO
+                        end
+                    end
+                end
+            else
+                network["switch"][asset_name]["state"] = Dict{String,PMD.SwitchState}("closed"=>PMD.CLOSED, "open"=>PMD.OPEN)
+            end
+
+        else
+            @warn "event of type '$(event["event_type"])' is not yet supported in PowerModelsONM"
+        end
+    end
 end
