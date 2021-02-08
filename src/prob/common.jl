bc7d31bd8d930baf8a268484630e07583ce56d54
David M Fobes <dfobes@lanl.gov>
2021-02-08 13:02:26 -0700

    REF: optimize_switches!

--- src/prob/common.jl
+++ src/prob/common.jl
@@ -1,35 +1,36 @@
-function optimize_switches!(mn_data_math::Dict{String,Any})
-    cbc_solver = PMD.optimizer_with_attributes(Cbc.Optimizer, "logLevel"=>0)
-    ipopt_solver = PMD.optimizer_with_attributes(Ipopt.Optimizer, "print_level"=>0, "tol"=>1e-6)
+function optimize_switches!(mn_data_math::Dict{String,Any}; solution_processors::Vector=[])
+    cbc_solver = PMD.optimizer_with_attributes(Cbc.Optimizer, "logLevel"=>0, "threads"=>4)
+    ipopt_solver = PMD.optimizer_with_attributes(Ipopt.Optimizer, "print_level"=>0, "tol"=>1e-4, "mu_strategy"=>"adaptive")
     juniper_solver = PMD.optimizer_with_attributes(Juniper.Optimizer, "nl_solver"=>ipopt_solver, "mip_solver"=>cbc_solver, "log_levels"=>[])
 
-    results = Dict{String,Any}()
+    # gurobi_solver = Gurobi.Optimizer(GRB_ENV)
+    # PMD.JuMP.set_optimizer_attribute(gurobi_solver, "OutputFlag", 0)
+
+    results = []
     for (n,nw) in mn_data_math["nw"]
         nw["per_unit"] = mn_data_math["per_unit"]
-        results[n] = run_mc_osw_mi(nw, PMD.LPUBFDiagPowerModel, juniper_solver)
+        if !isempty(results)
+            update_start_values!(nw, results[end]["solution"])
+            update_switch_settings!(nw, results[end]["solution"])
+        end
+        push!(results, run_mc_osw_mi(nw, PMD.LPUBFDiagPowerModel, juniper_solver; solution_processors=solution_processors))
     end
 
-    for (n, result) in results
-        for (l, switch) in result["solution"]["switch"]
-            mn_data_math["nw"][n]["switch"][l]["state"] = switch["state"]
-        end
-        for (l, gen) in result["solution"]["gen"]
-            for (k,v) in gen
-                mn_data_math["nw"][n]["gen"][l][k] = v
-            end
-        end
-        for (l, strg) in result["solution"]["storage"]
-            for (k,v) in strg
-                mn_data_math["nw"][n]["storage"][l][k] = v
-            end
-        end
-    end
+    solution = Dict("nw" => Dict("$n" => result["solution"] for (n, result) in enumerate(results)))
+
+    # results = run_mn_mc_osw_mi(mn_data_math, PMD.LPUBFDiagPowerModel, juniper_solver; solution_processors=solution_processors)
+    # solution = results["solution"]
+
+    update_start_values!(mn_data_math, solution)
+    update_switch_settings!(mn_data_math, solution)
+
+    return results
 end
 
 
 ""
-function solve_problem(problem::Function, data_math::Dict{String,<:Any}, form, solver)::Dict{String,Any}
-    return problem(data_math, form, solver; multinetwork=haskey(data_math, "nw"), make_si=false, solution_processors=[PMD.sol_data_model!])
+function solve_problem(problem::Function, data_math::Dict{String,<:Any}, form, solver; solution_processors::Vector=[])::Dict{String,Any}
+    return problem(data_math, form, solver; multinetwork=haskey(data_math, "nw"), make_si=false, solution_processors=solution_processors)
 end
 
 
