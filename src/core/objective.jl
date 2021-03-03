a95175484867ae2dc1dc8f84cac2ffa8e1ba77db
David M Fobes <dfobes@lanl.gov>
2021-03-03 08:14:30 -0700

    UPD: tuned mld+osw objective
    Added back disincentive to switch settings, will probably need to be
    tuned more later

--- src/core/objective.jl
+++ src/core/objective.jl
@@ -46,9 +46,9 @@ function objective_mc_min_load_setpoint_delta_simple_switch(pm::PMD._PM.Abstract
     PMD.JuMP.@objective(pm.model, Min,
         sum(
             sum( ((1 - PMD.var(pm, n, :z_demand, i))) for i in keys(nw_ref[:load])) +
-            sum( ((1 - PMD.var(pm, n, :z_shunt, i))) for (i,shunt) in nw_ref[:shunt])
+            sum( ((1 - PMD.var(pm, n, :z_shunt, i))) for (i,shunt) in nw_ref[:shunt]) +
             # sum( PMD.var(pm, n, :switch_state, l) for l in PMD.ids(pm, n, :switch_dispatchable)) +
-            # 1e-3 * sum( (state_start[(n,l)] - PMD.var(pm, n, :switch_state, l)) * (round(state_start[(n,l)]) == 0 ? -1 : 1) for l in PMD.ids(pm, n, :switch_dispatchable))
+            1e-3 * sum( (state_start[(n,l)] - PMD.var(pm, n, :switch_state, l)) * (round(state_start[(n,l)]) == 0 ? -1 : 1) for l in PMD.ids(pm, n, :switch_dispatchable))
         for (n, nw_ref) in PMD.nws(pm))
     )
 end
