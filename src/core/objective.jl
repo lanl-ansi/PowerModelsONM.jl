89b3179e06738cc4b11cbffd04e424e379f49494
David M Fobes <dfobes@lanl.gov>
2021-03-04 12:39:10 -0700

    FIX: mld problem
    The "simple" mld problem was not correctly shedding load, so this upgrades to the full mld problem contained in PMD to fix the problem
    
    loads now shed correctly in iowa240 test case with gurobi as solver.

--- src/core/objective.jl
+++ src/core/objective.jl
@@ -52,3 +52,49 @@ function objective_mc_min_load_setpoint_delta_simple_switch(pm::PMD._PM.Abstract
         for (n, nw_ref) in PMD.nws(pm))
     )
 end
+
+
+"minimum load delta objective (continuous load shed) with storage"
+function objective_mc_min_load_setpoint_delta_switch(pm::PMD._PM.AbstractPowerModel)
+    for (n, nw_ref) in PMD.nws(pm)
+        PMD.var(pm, n)[:delta_pg] = Dict(i => PMD.JuMP.@variable(pm.model,
+                [c in PMD.ref(pm, n, :gen, i)["connections"]], base_name="$(n)_$(i)_delta_pg",
+                start = 0.0) for i in PMD.ids(pm, n, :gen))
+
+                PMD.var(pm, n)[:delta_ps] = Dict(i => PMD.JuMP.@variable(pm.model,
+                [c in PMD.ref(pm, n, :storage, i)["connections"]], base_name="$(n)_$(i)_delta_ps",
+                start = 0.0) for i in PMD.ids(pm, n, :storage))
+
+        for (i, gen) in nw_ref[:gen]
+            for (idx, c) in enumerate(gen["connections"])
+                PMD.JuMP.@constraint(pm.model, PMD.var(pm, n, :delta_pg, i)[c] >=  (gen["pg"][idx] - PMD.var(pm, n, :pg, i)[c]))
+                PMD.JuMP.@constraint(pm.model, PMD.var(pm, n, :delta_pg, i)[c] >= -(gen["pg"][idx] - PMD.var(pm, n, :pg, i)[c]))
+            end
+        end
+
+        for (i, strg) in nw_ref[:storage]
+            for (idx, c) in enumerate(strg["connections"])
+                PMD.JuMP.@constraint(pm.model, PMD.var(pm, n, :delta_ps, i)[c] >=  (strg["ps"][idx] - PMD.var(pm, n, :ps, i)[c]))
+                PMD.JuMP.@constraint(pm.model, PMD.var(pm, n, :delta_ps, i)[c] >= -(strg["ps"][idx] - PMD.var(pm, n, :ps, i)[c]))
+            end
+        end
+    end
+
+    state_start = Dict(
+        (n,l) => PMD.ref(pm, n, :switch, l, "state")
+        for (n, nw_ref) in PMD.nws(pm) for l in PMD.ids(pm, n, :switch)
+    )
+
+    w = Dict(n => Dict(i => 10*get(load, "weight", 1.0) for (i,load) in PMD.ref(pm, n, :load)) for n in PMD.nw_ids(pm))
+
+    PMD.JuMP.@objective(pm.model, Min,
+        sum(
+            sum(                      10*(1 - PMD.var(pm, n, :z_voltage, i)) for (i,bus) in nw_ref[:bus]) +
+            sum( w[n][i]*sum(load["pd"])*(1 - PMD.var(pm, n, :z_demand, i)) for (i,load) in nw_ref[:load]) +
+            sum(        sum(shunt["gs"])*(1 - PMD.var(pm, n, :z_shunt, i)) for (i,shunt) in nw_ref[:shunt]) +
+            sum( sum(                         PMD.var(pm, n, :delta_pg, i)[c] for (idx,c) in enumerate(gen["connections"])) for (i,gen)  in nw_ref[:gen]) +
+            sum( sum(                         PMD.var(pm, n, :delta_ps, i)[c] for (idx,c) in enumerate(strg["connections"])) for (i,strg) in nw_ref[:storage]) +
+            sum( 1e-6 * (state_start[(n,l)] - PMD.var(pm, n, :switch_state, l)) * (round(state_start[(n,l)]) == 0 ? -1 : 1) for l in PMD.ids(pm, n, :switch_dispatchable))
+        for (n, nw_ref) in PMD.nws(pm))
+    )
+end
--- src/prob/osw_mld.jl
+++ src/prob/osw_mld.jl
@@ -87,17 +87,23 @@ end
 
 "Multinetwork load shedding problem for Branch Flow model"
 function build_mc_osw_mld_mi(pm::PMD.AbstractUBFModels)
+    PMD.variable_mc_bus_voltage_indicator(pm; relax=false)
+    PMD.variable_mc_bus_voltage_on_off(pm)
+
     PMD.variable_mc_branch_current(pm)
     PMD.variable_mc_branch_power(pm)
     PMD.variable_mc_switch_power(pm)
     PMD.variable_mc_switch_state(pm; relax=false)
     PMD.variable_mc_transformer_power(pm)
-    PMD.variable_mc_generator_power(pm)
-    PMD.variable_mc_bus_voltage(pm)
+
+    PMD.variable_mc_gen_indicator(pm; relax=false)
+    PMD.variable_mc_generator_power_on_off(pm)
+
+    PMD.variable_mc_storage_indicator(pm, relax=false)
+    PMD.variable_mc_storage_power_mi_on_off(pm, relax=false)
 
     PMD.variable_mc_load_indicator(pm; relax=false)
     PMD.variable_mc_shunt_indicator(pm; relax=false)
-    PMD.variable_mc_storage_power_mi(pm; relax=false)
 
     PMD.constraint_mc_model_current(pm)
 
@@ -105,25 +111,28 @@ function build_mc_osw_mld_mi(pm::PMD.AbstractUBFModels)
         PMD.constraint_mc_theta_ref(pm, i)
     end
 
+    PMD.constraint_mc_bus_voltage_on_off(pm)
+
     for i in PMD.ids(pm, :gen)
-        PMD.constraint_mc_generator_power(pm, i)
+        PMD.constraint_mc_gen_power_on_off(pm, i)
     end
 
     for i in PMD.ids(pm, :bus)
-        constraint_mc_power_balance_shed(pm, i)
+        PMD.constraint_mc_power_balance_shed(pm, i)
     end
 
     for i in PMD.ids(pm, :storage)
         PMD._PM.constraint_storage_state(pm, i)
+        PMD._PM.constraint_storage_complementarity_mi(pm, i)
+        PMD.constraint_mc_storage_on_off(pm, i)
         PMD.constraint_mc_storage_losses(pm, i)
         PMD.constraint_mc_storage_thermal_limit(pm, i)
-        PMD._PM.constraint_storage_complementarity_mi(pm, i)
     end
 
     for i in PMD.ids(pm, :branch)
         PMD.constraint_mc_power_losses(pm, i)
-
         PMD.constraint_mc_model_voltage_magnitude_difference(pm, i)
+
         PMD.constraint_mc_voltage_angle_difference(pm, i)
 
         PMD.constraint_mc_thermal_limit_from(pm, i)
@@ -139,5 +148,6 @@ function build_mc_osw_mld_mi(pm::PMD.AbstractUBFModels)
         PMD.constraint_mc_transformer_power(pm, i)
     end
 
-    objective_mc_min_load_setpoint_delta_simple_switch(pm)
+    objective_mc_min_load_setpoint_delta_switch(pm)
 end
+
