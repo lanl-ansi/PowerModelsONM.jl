256b1c970ce1be525b36de0d8784c0f7035a2ebd
David M Fobes <dfobes@lanl.gov>
2021-04-06 08:21:52 -0600

    UPD: prepare_network_case to include new CLI arguments for bounds

--- src/io/inputs.jl
+++ src/io/inputs.jl
@@ -1,5 +1,5 @@
 ""
-function prepare_network_case(network_file::String; events::Vector{<:Dict{String,Any}}=Vector{Dict{String,Any}}([]), time_elapsed::Real=1.0)::Tuple{Dict{String,Any},Dict{String,Any}}
+function prepare_network_case(network_file::String; events::Vector{<:Dict{String,Any}}=Vector{Dict{String,Any}}([]), time_elapsed::Real=1.0, vad::Real=3.0, vm_lb::Real=0.9, vm_ub::Real=1.1, clpu_factor::Real=2.0)::Tuple{Dict{String,Any},Dict{String,Any}}
     data_dss = PMD.parse_dss(network_file)
 
     # TODO: explicitly support DELTA connected generators in LPUBFDiag
@@ -16,13 +16,14 @@ function prepare_network_case(network_file::String; events::Vector{<:Dict{String
     # Allow all loads to be sheddable
     for (_,load) in data_eng["load"]
         load["dispatchable"] = PMD.YES
+        load["clpu_factor"] = clpu_factor
     end
 
     data_eng["voltage_source"]["source"]["pg_lb"] = zeros(length(data_eng["voltage_source"]["source"]["connections"]))
     data_eng["time_elapsed"] = time_elapsed  # 24 hours by default, 1 hr steps
 
-    PMD.apply_voltage_bounds!(data_eng; vm_lb=0.8, vm_ub=1.2)
-    apply_voltage_angle_bounds!(data_eng, 5)
+    PMD.apply_voltage_bounds!(data_eng; vm_lb=vm_lb, vm_ub=vm_ub)
+    apply_voltage_angle_bounds!(data_eng, vad)
 
     adjust_line_limits!(data_eng)
 
