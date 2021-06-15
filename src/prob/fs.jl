b3b6c81dca229213ebbdc7a8bfb3d7342157a98b
David M Fobes <dfobes@lanl.gov>
2021-06-15 16:03:42 -0600

    REF: run_fault_studies!

--- src/prob/fs.jl
+++ src/prob/fs.jl
@@ -3,9 +3,15 @@
 
 Runs fault studies defined in faults.json. If no faults file is provided, it will automatically generate faults
 using `PowerModelsProtection.build_mc_fault_study`.
+
+It will convert storage to limited generators, since storage is not yet supported in IVRU models in PowerModelsProtection
+
+Uses [`run_fault_study`](@ref run_fault_study) to solve the actual fault study.
 """
 function run_fault_studies!(args::Dict{String,<:Any})::Dict{String,Any}
     @info "Running fault studies"
+
+    network = _prepare_fault_study_multinetwork_data(args["network"])
 
     if !isempty(get(args, "faults", ""))
         if isa(args["faults"], String)
@@ -17,14 +23,36 @@ function run_fault_studies!(args::Dict{String,<:Any})::Dict{String,Any}
 
     fault_studies_results = Dict{String,Any}()
     @showprogress for n in sort([parse(Int,i) for i in keys(args["network"]["nw"])])
-        nw = deepcopy(args["network"]["nw"]["$n"])
-        nw["data_model"] = args["network"]["data_model"]
-        nw["method"] = "PMD"
-
-        convert_storage!(nw)
-
-        fault_studies_results["$n"] = PowerModelsProtection.solve_mc_fault_study(nw, args["faults"], args["nlp_solver"])
+        fault_studies_results["$n"] = run_fault_study(network["nw"]["$n"], args["faults"], args["juniper_solver"])
     end
 
     args["fault_studies_results"] = fault_studies_results
+end
+
+
+"""
+    run_fault_study(subnetwork::Dict, faults::Dict, solver)::Dict
+
+Uses `PowerModelsProtection.solve_mc_fault_study` to solve multiple faults defined in `faults`, applied
+to `subnetwork`, i.e., not a multinetwork, using a nonlinear `solver`.
+
+Requires the use of `PowerModelsDistribution.IVRUPowerModel`.
+"""
+function run_fault_study(subnetwork::Dict{String,<:Any}, faults::Dict{String,<:Any}, solver)::Dict{String,Any}
+    PowerModelsProtection.solve_mc_fault_study(subnetwork, faults, solver)
+end
+
+
+"helper function that helps to prepare all of the subnetworks for use in `PowerModelsProtection.solve_mc_fault_study`"
+function _prepare_fault_study_multinetwork_data(network::Dict)
+    mn_data = deepcopy(network)
+
+    PMD.apply_pmd!(convert_storage!, mn_data; apply_to_subnetworks=true)
+
+    for (n,nw) in mn_data["nw"]
+        nw["data_model"] = PMD.ENGINEERING
+        nw["method"] = "PMD"
+    end
+
+    return mn_data
 end
