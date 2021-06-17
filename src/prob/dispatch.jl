01402d933aea67a39c003a81736023d154aed7de
David M Fobes <dfobes@lanl.gov>
2021-06-17 16:41:24 -0600

    ADD: solver specification to optimize_dispatch!

--- src/prob/dispatch.jl
+++ src/prob/dispatch.jl
@@ -8,10 +8,10 @@ switch states from those results have already been propagated into `args["networ
 If `update_network_data` (default: true) the results of the optimization will be automatically merged into
 `args["network"]`.
 """
-function optimize_dispatch!(args::Dict{String,<:Any}; update_network_data::Bool=true)::Dict{String,Any}
+function optimize_dispatch!(args::Dict{String,<:Any}; update_network_data::Bool=true, solver::String="nlp_solver")::Dict{String,Any}
     args["opt-disp-formulation"] = _get_formulation(get(args, "opt-disp-formulation", "lindistflow"))
 
-    args["optimal_dispatch_result"] = optimize_dispatch(args["network"], args["opt-disp-formulation"], args["juniper_solver"])
+    args["optimal_dispatch_result"] = optimize_dispatch(args["network"], args["opt-disp-formulation"], args[solver])
 
     update_network_data && PMD._IM.update_data!(args["network"], get(args["optimal_dispatch_result"], "solution", Dict{String, Any}()))
 
