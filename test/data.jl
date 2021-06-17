8728c65e0d8926305abac70174a7c3bf4f5edea0
David M Fobes <dfobes@lanl.gov>
2021-06-17 16:46:41 -0600

    ADD: data tests

--- test/data.jl
+++ test/data.jl
@@ -1,3 +1,9 @@
 @testset "data handling checks" begin
+    base_network, network = parse_network("../test/data/IEEE13Nodeckt_mod.dss")
+    events = parse_events("../test/data/events.json", network)
+    network = apply_events(network, events)
 
+    math = PMD.transform_data_model(network)
+
+    @test !all(values(identify_cold_loads(math["nw"]["1"])))
 end
