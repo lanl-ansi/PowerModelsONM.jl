8e3265c5c3f23570787bfabb0b57a1e337459faf
David M Fobes <dfobes@lanl.gov>
2021-06-18 09:53:51 -0600

    ADD: stubs for stats unit tests

--- test/stats.jl
+++ test/stats.jl
@@ -1,3 +1,32 @@
 @testset "test statistical analysis functions" begin
+    args = Dict{String,Any}(
+        "network" => "../test/data/IEEE13Nodeckt_mod.dss",
+        "events" => "../test/data/events.json",
+        "settings" => "../test/data/settings.json",
+        "inverters" => "../test/data/inverters.json",
+        "faults" => "../test/data/faults.json",
+        "quiet" => true
+    )
 
+    args = entrypoint(args)
+
+    @testset "test action stats" begin
+
+    end
+
+    @testset "test dispatch stats" begin
+
+    end
+
+    @testset "test fault stats" begin
+
+    end
+
+    @testset "test microgrid stats" begin
+
+    end
+
+    @testset "test stability stats" begin
+
+    end
 end
