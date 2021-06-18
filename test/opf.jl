496309a614c384a63821bb932dc608fc2edaad5d
David M Fobes <dfobes@lanl.gov>
2021-06-18 09:53:08 -0600

    ADD: optimal dispatch unit tests

--- test/opf.jl
+++ test/opf.jl
@@ -10,5 +10,28 @@
 
     optimize_dispatch!(args)
 
-    @test isapprox(args["optimal_dispatch_result"]["objective"], 10.73; atol=1e-2)
+    @test isapprox(args["optimal_dispatch_result"]["objective"], 5.698; atol=1e-2)
+
+    args["opt-disp-formulation"] = "lindistflow"
+    optimize_dispatch!(args)
+
+    @test isapprox(args["optimal_dispatch_result"]["objective"], 5.369; atol=1e-2)
+
+    args["opt-disp-formulation"] = "acr"
+    optimize_dispatch!(args)
+
+    @test isapprox(args["optimal_dispatch_result"]["objective"], 5.780; atol=1e-2)
+    @test all(isapprox.(args["optimal_dispatch_result"]["solution"]["nw"]["1"]["bus"]["700"]["vm"], [2.46807, 2.54727, 2.57091]; atol=1e-2))
+    @test all(isapprox.(args["optimal_dispatch_result"]["solution"]["nw"]["1"]["bus"]["700"]["va"], [-0.734344, -120.934, 120.246]; atol=1e-2))
+    @test all(isapprox.(args["optimal_dispatch_result"]["solution"]["nw"]["1"]["bus"]["671"]["vm"], [2.4399, 2.58481, 2.56149]; atol=1e-2))
+    @test all(isapprox.(args["optimal_dispatch_result"]["solution"]["nw"]["1"]["bus"]["671"]["va"], [-2.0762, -121.026, 120.212]; atol=1e-2))
+
+    args["opt-disp-formulation"] = "acp"
+    optimize_dispatch!(args)
+
+    @test isapprox(args["optimal_dispatch_result"]["objective"], 5.780; atol=1e-2)
+    @test all(isapprox.(args["optimal_dispatch_result"]["solution"]["nw"]["1"]["bus"]["700"]["vm"], [2.46807, 2.54727, 2.57091]; atol=1e-2))
+    @test all(isapprox.(args["optimal_dispatch_result"]["solution"]["nw"]["1"]["bus"]["700"]["va"], [-0.734344, -120.934, 120.246]; atol=1e-2))
+    @test all(isapprox.(args["optimal_dispatch_result"]["solution"]["nw"]["1"]["bus"]["671"]["vm"], [2.4399, 2.58481, 2.56149]; atol=1e-2))
+    @test all(isapprox.(args["optimal_dispatch_result"]["solution"]["nw"]["1"]["bus"]["671"]["va"], [-2.0762, -121.026, 120.212]; atol=1e-2))
 end
