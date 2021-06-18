a100bbfcac8e29f806059c5d9b1f09b7ac218e2a
David M Fobes <dfobes@lanl.gov>
2021-06-18 09:53:21 -0600

    ADD: optimal switching unit tests

--- test/osw.jl
+++ test/osw.jl
@@ -3,10 +3,18 @@
         "network" => "../test/data/IEEE13Nodeckt_mod.dss",
         "events" => "../test/data/events.json",
     )
+    initialize_output!(args)
 
     parse_network!(args)
     parse_events!(args)
+    build_solver_instances!(args)
 
-    # TODO enable optimize switches test
-    # optimize_switches!(args)
+    optimize_switches!(args)
+
+    @test isapprox(args["optimal_switching_results"]["1"]["objective"], 0.001621; atol=1e-4)
+    @test isapprox(args["optimal_switching_results"]["2"]["objective"], 0.001434; atol=1e-4)
+
+    actions = get_timestep_device_actions!(args)
+    @test actions[1]["Switch configurations"]["671700"] == "open"
+    @test actions[2]["Switch configurations"]["671700"] == "closed"
 end
