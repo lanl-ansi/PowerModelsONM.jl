9737968a6d27bc7df0b4464247a7c0c422d8bd08
David M Fobes <dfobes@lanl.gov>
2021-06-21 11:26:04 -0600

    FIX: switch state power constraints

--- src/form/shared.jl
+++ src/form/shared.jl
@@ -1,9 +1,28 @@
 
+"Linear switch power on/off constraint for LPUBFDiagModel"
+function PowerModelsDistribution.constraint_mc_switch_state_on_off(pm::PMD.LPUBFDiagModel, nw::Int, i::Int, f_bus::Int, t_bus::Int, f_connections::Vector{Int}, t_connections::Vector{Int}; relax::Bool=false)
+    w_fr = PMD.var(pm, nw, :w, f_bus)
+    w_to = PMD.var(pm, nw, :w, t_bus)
 
+    f_bus = PMD.ref(pm, nw, :bus, f_bus)
+    t_bus = PMD.ref(pm, nw, :bus, t_bus)
 
+    f_vmax = f_bus["vmax"][[findfirst(isequal(c), f_bus["terminals"]) for c in f_connections]]
+    t_vmax = t_bus["vmax"][[findfirst(isequal(c), t_bus["terminals"]) for c in t_connections]]
 
+    vmax = min.(fill(2.0, length(f_bus["vmax"])), f_vmax, t_vmax)
 
+    z = PMD.var(pm, nw, :switch_state, i)
 
+    for (idx, (fc, tc)) in enumerate(zip(f_connections, t_connections))
+        if relax
+            JuMP.@constraint(pm.model, w_fr[fc] - w_to[tc] <=  vmax[idx].^2 * (1-z))
+            JuMP.@constraint(pm.model, w_fr[fc] - w_to[tc] >= -vmax[idx].^2 * (1-z))
+        else
+            JuMP.@constraint(pm.model, z => {w_fr[fc] == w_to[tc]})
+        end
+    end
+end
 
 
 "Linear switch power on/off constraint for LPUBFDiagModel"
@@ -17,36 +36,19 @@ function PowerModelsDistribution.constraint_mc_switch_power_on_off(pm::PMD.LPUBF
 
     connections = PMD.ref(pm, nw, :switch, i)["f_connections"]
 
-    rating = get(PMD.ref(pm, nw, :switch, i), "rate_a", fill(1e-2, length(connections)))
+    switch = PMD.ref(pm, nw, :switch, i)
+
+    rating = min.(fill(100.0, length(connections)), PMD._calc_branch_power_max_frto(switch, PMD.ref(pm, nw, :bus, f_bus), PMD.ref(pm, nw, :bus, t_bus))...)
 
     for (idx, c) in enumerate(connections)
         if relax
-            PMD.JuMP.@constraint(pm.model, psw[c] <=  rating[idx] * z)
-            PMD.JuMP.@constraint(pm.model, psw[c] >= -rating[idx] * z)
-            PMD.JuMP.@constraint(pm.model, qsw[c] <=  rating[idx] * z)
-            PMD.JuMP.@constraint(pm.model, qsw[c] >= -rating[idx] * z)
+            JuMP.@constraint(pm.model, psw[c] <=  rating[idx] * z)
+            JuMP.@constraint(pm.model, psw[c] >= -rating[idx] * z)
+            JuMP.@constraint(pm.model, qsw[c] <=  rating[idx] * z)
+            JuMP.@constraint(pm.model, qsw[c] >= -rating[idx] * z)
         else
-            PMD.JuMP.@constraint(pm.model, !z => {psw[c] == 0.0})
-            PMD.JuMP.@constraint(pm.model, !z => {qsw[c] == 0.0})
-        end
-    end
-end
-
-
-"Linear switch state on/off constraint for LPUBFDiagModel"
-function PowerModelsDistribution.constraint_mc_switch_state_on_off(pm::PMD.LPUBFDiagModel, nw::Int, i::Int, f_bus::Int, t_bus::Int, f_connections::Vector{Int}, t_connections::Vector{Int}; relax::Bool=false)
-    w_fr = PMD.var(pm, nw, :w, f_bus)
-    w_to = PMD.var(pm, nw, :w, t_bus)
-
-    z = PMD.var(pm, nw, :switch_state, i)
-
-    for (fc, tc) in zip(f_connections, t_connections)
-        if relax
-            M = 0.2
-            PMD.JuMP.@constraint(pm.model, w_fr[fc] - w_to[tc] <=  M * (1-z))
-            PMD.JuMP.@constraint(pm.model, w_fr[fc] - w_to[tc] >= -M * (1-z))
-        else
-            PMD.JuMP.@constraint(pm.model, z => {w_fr[fc] == w_to[tc]})
+            JuMP.@constraint(pm.model, !z => {psw[c] == 0.0})
+            JuMP.@constraint(pm.model, !z => {qsw[c] == 0.0})
         end
     end
 end
