6779a010371730cb73a4b304a7da76ecbc3ddb14
David M Fobes <dfobes@lanl.gov>
2021-06-21 11:29:16 -0600

    FIX: dispatch tests

--- test/opf.jl
+++ test/opf.jl
@@ -2,36 +2,36 @@
     args = Dict{String,Any}(
         "network" => "../test/data/IEEE13Nodeckt_mod.dss",
         "events" => "../test/data/events.json",
-        "opt-disp-formulation" => "nfa",
     )
 
     parse_network!(args)
     build_solver_instances!(args)
 
+    args["opt-disp-formulation"] = "nfa"
     optimize_dispatch!(args)
 
-    @test isapprox(args["optimal_dispatch_result"]["objective"], 5.698; atol=1e-2)
+    @test isapprox(args["optimal_dispatch_result"]["objective"], 6.893; atol=1e-2)
 
     args["opt-disp-formulation"] = "lindistflow"
     optimize_dispatch!(args)
 
-    @test isapprox(args["optimal_dispatch_result"]["objective"], 5.369; atol=1e-2)
+    @test isapprox(args["optimal_dispatch_result"]["objective"], 6.554; atol=1e-2)
 
     args["opt-disp-formulation"] = "acr"
     optimize_dispatch!(args)
 
-    @test isapprox(args["optimal_dispatch_result"]["objective"], 5.780; atol=1e-2)
-    @test all(isapprox.(args["optimal_dispatch_result"]["solution"]["nw"]["1"]["bus"]["700"]["vm"], [2.46807, 2.54727, 2.57091]; atol=1e-2))
-    @test all(isapprox.(args["optimal_dispatch_result"]["solution"]["nw"]["1"]["bus"]["700"]["va"], [-0.734344, -120.934, 120.246]; atol=1e-2))
-    @test all(isapprox.(args["optimal_dispatch_result"]["solution"]["nw"]["1"]["bus"]["671"]["vm"], [2.4399, 2.58481, 2.56149]; atol=1e-2))
-    @test all(isapprox.(args["optimal_dispatch_result"]["solution"]["nw"]["1"]["bus"]["671"]["va"], [-2.0762, -121.026, 120.212]; atol=1e-2))
+    @test isapprox(args["optimal_dispatch_result"]["objective"], 6.979; atol=1e-2)
+    @test all(isapprox.(args["optimal_dispatch_result"]["solution"]["nw"]["1"]["bus"]["700"]["vm"], [2.4676, 2.5471, 2.5706]; atol=1e-2))
+    @test all(isapprox.(args["optimal_dispatch_result"]["solution"]["nw"]["1"]["bus"]["700"]["va"], [-0.7483, -120.9461, 120.2302]; atol=1e-2))
+    @test all(isapprox.(args["optimal_dispatch_result"]["solution"]["nw"]["1"]["bus"]["671"]["vm"], [2.4394, 2.5846, 2.5612]; atol=1e-2))
+    @test all(isapprox.(args["optimal_dispatch_result"]["solution"]["nw"]["1"]["bus"]["671"]["va"], [-2.0905, -121.0381, 120.1962]; atol=1e-2))
 
     args["opt-disp-formulation"] = "acp"
     optimize_dispatch!(args)
 
-    @test isapprox(args["optimal_dispatch_result"]["objective"], 5.780; atol=1e-2)
-    @test all(isapprox.(args["optimal_dispatch_result"]["solution"]["nw"]["1"]["bus"]["700"]["vm"], [2.46807, 2.54727, 2.57091]; atol=1e-2))
-    @test all(isapprox.(args["optimal_dispatch_result"]["solution"]["nw"]["1"]["bus"]["700"]["va"], [-0.734344, -120.934, 120.246]; atol=1e-2))
-    @test all(isapprox.(args["optimal_dispatch_result"]["solution"]["nw"]["1"]["bus"]["671"]["vm"], [2.4399, 2.58481, 2.56149]; atol=1e-2))
-    @test all(isapprox.(args["optimal_dispatch_result"]["solution"]["nw"]["1"]["bus"]["671"]["va"], [-2.0762, -121.026, 120.212]; atol=1e-2))
+    @test isapprox(args["optimal_dispatch_result"]["objective"], 6.979; atol=1e-2)
+    @test all(isapprox.(args["optimal_dispatch_result"]["solution"]["nw"]["1"]["bus"]["700"]["vm"], [2.4676, 2.5471, 2.5706]; atol=1e-2))
+    @test all(isapprox.(args["optimal_dispatch_result"]["solution"]["nw"]["1"]["bus"]["700"]["va"], [-0.7483, -120.9461, 120.2302]; atol=1e-2))
+    @test all(isapprox.(args["optimal_dispatch_result"]["solution"]["nw"]["1"]["bus"]["671"]["vm"], [2.4394, 2.5846, 2.5612]; atol=1e-2))
+    @test all(isapprox.(args["optimal_dispatch_result"]["solution"]["nw"]["1"]["bus"]["671"]["va"], [-2.0905, -121.0381, 120.1962]; atol=1e-2))
 end
