b1cd1fe853488a81a2a7fb23de9180f731258c4b
David M Fobes <dfobes@lanl.gov>
2021-06-22 16:37:42 -0600

    REF: logging

--- src/core/logging.jl
+++ src/core/logging.jl
@@ -1,47 +1,9 @@
 """
-    silence!(mod::Module)
-
-Sets loglevel for Module `mod` to `:Error`, silencing Info and Warn
-"""
-function silence!(mod)
-    set_logging_level!(mod, :Error)
-end
-
-
-"Helper function to set loglevel of ONM to :Error"
-silence!() = silence!(PowerModelsONM)
-
-
-"""
-    reset_logging_level!()
-
-Resets the log level to Info
-"""
-function reset_logging_level!()
-    Logging.global_logger(Logging.ConsoleLogger(; meta_formatter=PowerModelsDistribution._pmd_metafmt))
-end
-
-
-"""
-    set_logging_level!(mod::Module, level::Symbol)
-
-Sets the logging level for Module `mod`: `:Info`, `:Warn`, `:Error`
-"""
-function set_logging_level!(mod::Module, level::Symbol)
-    Logging.global_logger(_make_filtered_logger(mod, getfield(Logging, level)))
-end
-
-
-"Helper function to set logging level of ONM"
-set_logging_level!(level::Symbol) = set_logging_level!(PowerModelsONM, level)
-
-
-"""
-    _make_filtered_logger(mod::Module, level::Logging.LogLevel)
+    _make_filtered_logger(mods::Vector, level::Logging.LogLevel)
 
 Helper function to create the filtered logger for PMD
 """
-function _make_filtered_logger(mods::Vector, level::Logging.LogLevel)
+function _make_filtered_logger(mods::Vector{<:Module}, level::Logging.LogLevel)
     LoggingExtras.EarlyFilteredLogger(_LOGGER) do log
         if any(log._module == mod for mod in mods) && log.level < level
             return false
@@ -53,41 +15,39 @@ end
 
 
 """
-    _make_filtered_logger(mod::Module, level::Logging.LogLevel)
-
-Helper function to create the filtered logger for PMD
-"""
-function _make_filtered_logger(mod::Module, level::Logging.LogLevel)
-    LoggingExtras.EarlyFilteredLogger(_LOGGER) do log
-        if log._module == mod && log.level < level
-            return false
-        else
-            return true
-        end
-    end
-end
-
-
-"Helper function to create the filtered logger for ONM"
-_make_filtered_logger(level) = _make_filtered_logger(PowerModelsONM, level)
-
-
-"""
     setup_logging!(args::Dict{String,<:Any})
 
 Configures logging based on runtime arguments, for use inside [`entrypoint`](@ref entrypoint)
 """
 function setup_logging!(args::Dict{String,<:Any})
-    mods = [PowerModelsDistribution, PowerModelsProtection, PowerModelsStability, Juniper, JSONSchema]
     if get(args, "quiet", false)
+        set_log_level!(:Error)
+    elseif get(args, "verbose", false)
+        set_log_level!(:Info)
+    elseif get(args, "debug", false)
+        set_log_level!(:Debug)
+    else
+        set_log_level!(:Warn)
+    end
+end
+
+
+"""
+    set_log_level!(level::Symbol)
+
+Configures logging based `level`, `:Error`, `:Warn`, `:Info`, or `:Debug`
+"""
+function set_log_level!(level::Symbol)
+    mods = [PowerModelsDistribution, PowerModelsProtection, PowerModelsStability, Juniper, JSONSchema]
+    if level == :Error
         loglevel = Logging.Error
         push!(mods, PowerModelsONM)
 
         # TODO remove need for Memento
         PMD._IM.silence()
-    elseif get(args, "verbose", false)
+    elseif level == :Info
         loglevel = Logging.Info
-    elseif get(args, "debug", false)
+    elseif level == :Debug
         loglevel = Logging.Debug
     else
         loglevel = Logging.Error
@@ -97,4 +57,14 @@ function setup_logging!(args::Dict{String,<:Any})
     end
 
     Logging.global_logger(_make_filtered_logger(mods, loglevel))
+end
+
+
+"""
+    silence!()
+
+Sets logging level to "quiet"
+"""
+function silence!()
+    set_log_level!(:Error)
 end
