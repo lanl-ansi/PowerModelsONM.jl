e6fc43a00da6542492438638c2508c091eb1acd5
David M Fobes <dfobes@lanl.gov>
2021-11-09 09:45:19 -0700

    ADD: switch constraints for NFAUPowerModel

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,7 @@
 
 ## staged
 
+- Added `constraint_mc_switch_state_on_off` and `constraint_mc_switch_power_on_off` for `NFAUPowerModel`
 - Updated `apply_settings` to use `set_time_elapsed!` from PowerModelsDistribution
 - Fixed `_find_nw_id_from_timestep` for updates from PowerModelsDistribution
 - Added `JuMP.lower_bound(x)` and `JuMP.upper_bound(x)` for `x::Float64`
--- src/form/shared.jl
+++ src/form/shared.jl
@@ -75,6 +75,51 @@ function PowerModelsDistribution.constraint_mc_switch_power_on_off(pm::PMD.LPUBF
 end
 
 
+@doc raw"""
+    constraint_mc_switch_state_on_off(pm::PMD.AbstractUnbalancedActivePowerModel, nw::Int, i::Int, f_bus::Int, t_bus::Int, f_connections::Vector{Int}, t_connections::Vector{Int}; relax::Bool=false)
+
+No voltage variables, do nothing
+"""
+function PowerModelsDistribution.constraint_mc_switch_state_on_off(pm::PMD.AbstractUnbalancedActivePowerModel, nw::Int, i::Int, f_bus::Int, t_bus::Int, f_connections::Vector{Int}, t_connections::Vector{Int}; relax::Bool=false)
+end
+
+
+@doc raw"""
+    constraint_mc_switch_power_on_off(pm::PMD.AbstractUnbalancedActivePowerModel, nw::Int, f_idx::Tuple{Int,Int,Int}; relax::Bool=false)
+
+Linear switch power on/off constraint for Active Power Only Models. If `relax`, an [indicator constraint](https://jump.dev/JuMP.jl/stable/manual/constraints/#Indicator-constraints) is used.
+
+```math
+\begin{align}
+& P^{sw}_{i,c} \leq P^{swu}_{i,c} z^{sw}_i\ \forall i \in P,\forall c \in C \\
+& P^{sw}_{i,c} \geq -P^{swu}_{i,c} z^{sw}_i\ \forall i \in P,\forall c \in C
+\end{align}
+```
+"""
+function PowerModelsDistribution.constraint_mc_switch_power_on_off(pm::PMD.AbstractUnbalancedActivePowerModel, nw::Int, f_idx::Tuple{Int,Int,Int}; relax::Bool=false)
+    i, f_bus, t_bus = f_idx
+
+    psw = PMD.var(pm, nw, :psw, f_idx)
+
+    z = PMD.var(pm, nw, :switch_state, i)
+
+    connections = PMD.ref(pm, nw, :switch, i)["f_connections"]
+
+    switch = PMD.ref(pm, nw, :switch, i)
+
+    rating = min.(fill(100.0, length(connections)), PMD._calc_branch_power_max_frto(switch, PMD.ref(pm, nw, :bus, f_bus), PMD.ref(pm, nw, :bus, t_bus))...)
+
+    for (idx, c) in enumerate(connections)
+        if relax
+            JuMP.@constraint(pm.model, psw[c] <=  rating[idx] * z)
+            JuMP.@constraint(pm.model, psw[c] >= -rating[idx] * z)
+        else
+            JuMP.@constraint(pm.model, !z => {psw[c] == 0.0})
+        end
+    end
+end
+
+
 "helper function for pd/qd variables"
 function JuMP.lower_bound(x::Float64)
     return x
