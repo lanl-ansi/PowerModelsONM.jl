f997284f9fe7e4669bd97b175df23aaef2af6196
David M Fobes <dfobes@lanl.gov>
2022-03-31 09:59:17 -0600

    UPD: unit tests

--- test/faults.jl
+++ test/faults.jl
@@ -3,17 +3,19 @@
         "network" => "../test/data/ieee13_feeder.dss",
         "settings" => "../test/data/ieee13_settings.json",
         "faults" => "../test/data/ieee13_faults.json",
-        "skip" => ["stability", "dispatch", "switching"],
+        "quiet" => true,
+        "opt-dispatch-formulation" => "acp",
+        "skip" => ["stability"],
     )
     entrypoint(args)
 
-    @test all(isapprox.(args["fault_studies_results"]["1"]["702"]["3p"]["1"]["solution"]["fault"]["1"]["cf"], [3747.46, 2675.6, 1505.02]; atol=1e2))
-    @test all(isapprox.(args["fault_studies_results"]["1"]["702"]["ll"]["1"]["solution"]["fault"]["1"]["cf"], [9781.77, 9781.77]; atol=1e2))
-    @test all(isapprox.(args["fault_studies_results"]["1"]["702"]["lg"]["1"]["solution"]["fault"]["1"]["cf"], [10370.8]; atol=1e2))
+    @test all(isapprox.(args["fault_studies_results"]["8"]["702"]["3p"]["1"]["solution"]["fault"]["1"]["cf"], [11783.8, 11723.8, 10337.1]; atol=1e3))
+    @test all(isapprox.(args["fault_studies_results"]["8"]["702"]["ll"]["1"]["solution"]["fault"]["1"]["cf"], [7492.97, 7492.97]; atol=1e3))
+    @test all(isapprox.(args["fault_studies_results"]["8"]["702"]["lg"]["1"]["solution"]["fault"]["1"]["cf"], [6395.87]; atol=1e2))
 
-    @test all(isapprox.(args["output_data"]["Fault currents"][1]["702"]["3p"]["1"]["switch"]["671700"]["|I| (A)"], [4948.48, 5062.14, 4690.72]; atol=1e1))
-    @test all(isapprox.(args["output_data"]["Fault currents"][1]["702"]["ll"]["1"]["switch"]["671700"]["|I| (A)"], [2649.87, 3388.17, 1086.72]; atol=1e1))
-    @test all(isapprox.(args["output_data"]["Fault currents"][1]["702"]["lg"]["1"]["switch"]["671700"]["|I| (A)"], [7925.37, 1602.05, 6577.87]; atol=1e1))
+    @test all(isapprox.(args["output_data"]["Fault currents"][8]["702"]["3p"]["1"]["switch"]["671700"]["|I| (A)"], [3377.51, 3407.76, 3111.84]; atol=1e2))
+    @test all(isapprox.(args["output_data"]["Fault currents"][8]["702"]["ll"]["1"]["switch"]["671700"]["|I| (A)"], [2380.69, 4488.38, 2481.67]; atol=1e2))
+    @test all(isapprox.(args["output_data"]["Fault currents"][8]["702"]["lg"]["1"]["switch"]["671700"]["|I| (A)"], [2077.65, 1220.18, 1418.96]; atol=1e2))
 
-    @test all(isapprox.(args["output_data"]["Fault currents"][1]["702"]["lg"]["1"]["switch"]["671700"]["|V| (V)"], [2.25208, 4.19198, 2.11782]; atol=1e-2))
+    @test all(isapprox.(args["output_data"]["Fault currents"][8]["702"]["lg"]["1"]["switch"]["671700"]["|V| (V)"], [1.22373, 3.17669, 2.82304]; atol=1e-1))
 end
