8c8b2d6cb43bfdb6f94cbbc15a2697cbc442b90e
David M Fobes <dfobes@lanl.gov>
2022-06-06 11:06:04 -0600

    FIX: parse_dss_settings

--- src/io/settings.jl
+++ src/io/settings.jl
@@ -661,11 +661,11 @@ function parse_dss_settings(dss_settings::T, eng::T)::T where T <: Dict{String,A
             settings[eng_obj_type][eng_obj_id] = Dict{String,Any}()
         end
 
-        if haskey(obj, "enabled")
+        if haskey(obj, "enabled") && !isa(obj["enabled"], PMD.Status) && isa(obj["enabled"], String)
             settings[eng_obj_type][eng_obj_id]["status"] = parse(PMD.Status, lowercase(obj["enabled"]))
         end
 
-        if haskey(obj, "inverter")
+        if haskey(obj, "inverter") && !isa(obj["inverter"], Inverter) && isa(obj["inverter"], String)
             settings[eng_obj_type][eng_obj_id]["inverter"] = parse(Inverter, lowercase(obj["inverter"]))
         end
     end
