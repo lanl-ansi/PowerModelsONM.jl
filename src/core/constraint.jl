baaca2488fa689251f93591f4536902e7a99cbe6
David M Fobes <dfobes@lanl.gov>
2022-06-24 15:02:44 -0600

    ADD: strictly increasing constraint

--- src/core/constraint.jl
+++ src/core/constraint.jl
@@ -828,3 +828,19 @@ function constraint_disable_networking(pm::AbstractUnbalancedPowerModel, nw::Int
         end
     end
 end
+
+
+"""
+    constraint_energized_blocks_strictly_increasing(pm::AbstractUnbalancedPowerModel, n_1::Int, n_2::Int)
+
+Constraint to ensure that the number of energized load blocks from one timestep to another is strictly increasing
+and that once energized, a load block cannot be shed in a later timestep.
+"""
+function constraint_energized_blocks_strictly_increasing(pm::AbstractUnbalancedPowerModel, n_1::Int, n_2::Int)
+    for block_id in ids(pm, n_2, :blocks)
+        z_block_n1 = var(pm, n_1, :z_block, block_id)
+        z_block_n2 = var(pm, n_2, :z_block, block_id)
+
+        JuMP.@constraint(pm.model, z_block_n2 >= z_block_n1)
+    end
+end
