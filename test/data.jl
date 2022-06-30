1e314a34fa96f7297af77f6f197233104ebaad6f
David M Fobes <dfobes@lanl.gov>
2022-06-30 16:11:30 -0600

    ADD: data unit tests

--- test/data.jl
+++ test/data.jl
@@ -1,3 +1,12 @@
 @testset "data handling checks" begin
     @test PowerModelsONM._get_formulation(PMD.ACPUPowerModel) == PMD.ACPUPowerModel
 end
+
+@testset "test custom onm developer functions" begin
+    eng = parse_network("../test/data/ieee13_feeder.dss")[1]
+    math = transform_data_model(eng)
+
+    pm = instantiate_onm_model(eng, NFAUPowerModel, build_block_mld)
+
+    @test pm.data == math
+end
