4e34dcbda818db7db897dc13ecde98b325cdecd8
David M Fobes <dfobes@lanl.gov>
2022-07-05 14:53:01 -0600

    UPD: graphml tests

--- test/graphml.jl
+++ test/graphml.jl
@@ -8,7 +8,7 @@
 
        save_graphml("../test/data/ieee13_nested.graphml", eng; type="nested")
        open("../test/data/ieee13_nested.graphml", "r") do io
-            @test length(readlines(io)) == 1588
+            @test length(readlines(io)) == 1461
        end
        rm("../test/data/ieee13_nested.graphml")
     end
@@ -20,7 +20,7 @@
 
         save_graphml("../test/data/ieee13_unnested.graphml", eng; type="unnested")
         open("../test/data/ieee13_unnested.graphml", "r") do io
-             @test length(readlines(io)) == 1558
+             @test length(readlines(io)) == 1431
         end
         rm("../test/data/ieee13_unnested.graphml")
     end
