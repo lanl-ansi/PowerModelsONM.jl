be71d423e20289aaf7fe06fadc19a79b7ca55f01
David M Fobes <dfobes@lanl.gov>
2022-07-06 08:45:31 -0600

    UPD: solver settings for nlp tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,26 +13,26 @@ import HiGHS
 
 minlp_solver = optimizer_with_attributes(
     Juniper.Optimizer,
-    "nl_solver" => optimizer_with_attributes(Ipopt.Optimizer, "tol"=>1e-4, "mu_strategy"=>"adaptive", "print_level"=>0),
+    "nl_solver" => optimizer_with_attributes(Ipopt.Optimizer, "tol"=>1e-6, "mu_strategy"=>"adaptive", "print_level"=>0),
     "mip_solver" => optimizer_with_attributes(
         HiGHS.Optimizer,
         "presolve" => "off",
-        "primal_feasibility_tolerance" => 1e-4,
-        "dual_feasibility_tolerance" => 1e-4,
-        "mip_feasibility_tolerance" => 1e-4,
+        "primal_feasibility_tolerance" => 1e-6,
+        "dual_feasibility_tolerance" => 1e-6,
+        "mip_feasibility_tolerance" => 1e-6,
         "mip_rel_gap" => 0.0001,
         "small_matrix_value" => 1e-12,
         "allow_unbounded_or_infeasible" => true,
-        "output_flag" => false,
+        "output_flag" => true,
         "random_seed" => 1,
     ),
     "mip_gap" => 0.0001,
-    "atol" => 1e-3,
+    "atol" => 1e-6,
     "allow_almost_solved_integral" => true,
     "allow_almost_solved" => true,
     "feasibility_pump" => true,
     "seed" => 1,
-    "log_levels" => [],
+    "log_levels" => [:Info, :Table, :Options],
 )
 
 using Test
