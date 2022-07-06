c89e329503382fb9fb8b9efde6797bb13296671d
David M Fobes <dfobes@lanl.gov>
2022-07-06 07:54:14 -0600

    FIX: Adjust nlp test solver parameters

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,23 +13,25 @@ import HiGHS
 
 minlp_solver = optimizer_with_attributes(
     Juniper.Optimizer,
-    "nl_solver" => optimizer_with_attributes(Ipopt.Optimizer, "tol"=>1e-5, "mu_strategy"=>"adaptive", "print_level"=>0),
+    "nl_solver" => optimizer_with_attributes(Ipopt.Optimizer, "tol"=>1e-4, "mu_strategy"=>"adaptive", "print_level"=>0),
     "mip_solver" => optimizer_with_attributes(
         HiGHS.Optimizer,
-        "primal_feasibility_tolerance" => 1e-5,
-        "dual_feasibility_tolerance" => 1e-5,
+        "presolve" => "off",
+        "primal_feasibility_tolerance" => 1e-4,
+        "dual_feasibility_tolerance" => 1e-4,
         "mip_feasibility_tolerance" => 1e-4,
         "mip_rel_gap" => 0.0001,
         "small_matrix_value" => 1e-12,
         "allow_unbounded_or_infeasible" => true,
         "output_flag" => false,
+        "random_seed" => 1,
     ),
     "mip_gap" => 0.0001,
-    "atol" => 1e-4,
+    "atol" => 1e-3,
     "allow_almost_solved_integral" => true,
     "allow_almost_solved" => true,
     "feasibility_pump" => true,
-    "seed" => 0,
+    "seed" => 1,
     "log_levels" => [],
 )
 
