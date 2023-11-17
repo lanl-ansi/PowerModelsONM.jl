30393fa61d9296358ef8b76408b6fea6fec392cf
David M Fobes <dfobes@lanl.gov>
2023-11-16 18:21:38 -0700

    FIX: radiality constraint for multiple switches between blocks
    This fixes the edge case where there are more than one switch between the same two neighboring blocks.
    
    Adds unit tests to ensure that when radiality constraints are enabled that switches can be either all open, or only one can be closed.
    
    EzXML dependency change broke a unit test.
    
    Resolves #13

--- test/graphml.jl
+++ test/graphml.jl
@@ -8,7 +8,7 @@
 
        save_graphml("../test/data/ieee13_nested.graphml", eng; type="nested")
        open("../test/data/ieee13_nested.graphml", "r") do io
-            @test length(readlines(io)) == 1509
+            @test length(readlines(io)) == 1533
        end
        rm("../test/data/ieee13_nested.graphml")
     end
@@ -20,7 +20,7 @@
 
         save_graphml("../test/data/ieee13_unnested.graphml", eng; type="unnested")
         open("../test/data/ieee13_unnested.graphml", "r") do io
-             @test length(readlines(io)) == 1479
+             @test length(readlines(io)) == 1503
         end
         rm("../test/data/ieee13_unnested.graphml")
     end
--- test/mld.jl
+++ test/mld.jl
@@ -203,3 +203,76 @@
         end
     end
 end
+
+
+@testset "test radiality " begin
+    solver = build_solver_instances(;solver_options=Dict{String,Any}("HiGHS"=>Dict{String,Any}("output_flag"=>false, "presolve"=>"off")))["mip_solver"]
+    eng_s = parse_file("../test/data/network.ieee13mod.dss")
+
+    eng_s["time_elapsed"] = 1.0
+    eng_s["switch_close_actions_ub"] = Inf
+
+    PMD.apply_voltage_bounds!(eng_s; vm_lb=0.9, vm_ub=1.1)
+    PMD.apply_voltage_angle_difference_bounds!(eng_s, 10.0)
+    PMD.adjust_line_limits!(eng_s, Inf)
+    PMD.adjust_transformer_limits!(eng_s, Inf)
+
+    for switch in values(eng_s["switch"])
+        switch["dispatchable"] = YES
+        switch["state"] = CLOSED
+        switch["status"] = ENABLED
+    end
+
+    for t in ["storage", "solar", "generator"]
+        for obj in values(eng_s[t])
+            obj["inverter"] = GRID_FOLLOWING
+        end
+    end
+
+    set_option!(eng_s, ("options", "objective", "disable-load-block-weight-cost"), true)
+    set_option!(eng_s, ("options", "objective", "disable-storage-discharge-cost"), true)
+    set_option!(eng_s, ("options", "objective", "disable-generation-dispatch-cost"), true)
+
+    r = solve_block_mld(eng_s, LPUBFDiagPowerModel, solver)
+
+    @test r["solution"]["switch"]["680675"]["state"] != r["solution"]["switch"]["671692"]["state"]
+    @test length(filter(x->x.second["state"]==OPEN, r["solution"]["switch"])) == 2
+    @test r["objective"] < 1.0
+
+    eng_s["switch"]["680675"]["state"] = OPEN
+
+    r = solve_block_mld(eng_s, LPUBFDiagPowerModel, solver)
+
+    @test r["solution"]["switch"]["680675"]["state"] == OPEN
+    @test r["solution"]["switch"]["671692"]["state"] == CLOSED
+    @test length(filter(x->x.second["state"]==OPEN, r["solution"]["switch"])) == 2
+    @test r["objective"] < 1.0
+
+    eng_s["switch"]["680675"]["state"] = CLOSED
+    eng_s["switch"]["671692"]["state"] = OPEN
+
+    r = solve_block_mld(eng_s, LPUBFDiagPowerModel, solver)
+
+    @test r["solution"]["switch"]["680675"]["state"] == CLOSED
+    @test r["solution"]["switch"]["671692"]["state"] == OPEN
+    @test length(filter(x->x.second["state"]==OPEN, r["solution"]["switch"])) == 2
+    @test r["objective"] < 1.0
+
+    eng_s["switch"]["680675"]["state"] = OPEN
+    eng_s["switch"]["671692"]["state"] = OPEN
+
+    r = solve_block_mld(eng_s, LPUBFDiagPowerModel, solver)
+
+    @test (r["solution"]["switch"]["680675"]["state"] != r["solution"]["switch"]["671692"]["state"]) || (r["solution"]["switch"]["680675"]["state"] == OPEN)
+    @test length(filter(x->x.second["state"]==OPEN, r["solution"]["switch"])) == 2
+    @test r["objective"] < 1.0
+
+    eng_s["switch"]["680675"]["dispatchable"] = NO
+    eng_s["switch"]["671692"]["dispatchable"] = NO
+
+    r = solve_block_mld(eng_s, LPUBFDiagPowerModel, solver)
+
+    @test r["solution"]["switch"]["680675"]["state"] == r["solution"]["switch"]["671692"]["state"]
+    @test length(filter(x->x.second["state"]==OPEN, r["solution"]["switch"])) == 2
+    @test r["objective"] < 1.0
+end
