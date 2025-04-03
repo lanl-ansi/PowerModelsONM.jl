596ed56c5da8d3c542fc6e32965a2434a9489aa3
jjospina <jjospina@lanl.gov>
2025-04-03 12:15:28 -0600

    REF: ravens2math functions to use information from RAVENS JSON file. Do not add info if the info is not available.

--- src/data_model/ravens2math.jl
+++ src/data_model/ravens2math.jl
@@ -19,38 +19,85 @@ function ravens2math_add_root_passthrough_default!(data_math::Dict{String,<:Any}
 end
 
 function ravens2math_add_load_passthrough_default!(data_math::Dict{String,<:Any}, data_ravens::Dict{String,<:Any})
-    for (name, data) in get(data_math, "load", Dict{Any,Dict{String,Any}}())
-        data["priority"] = 1
+    for (name, load_data) in get(data_math, "load", Dict{Any,Dict{String,Any}}())
+        load_name = load_data["name"]
+        enrg_cnsmrs = data_ravens["PowerSystemResource"]["Equipment"]["ConductingEquipment"]["EnergyConnection"]["EnergyConsumer"]
+        enrg_cnsmr_data = enrg_cnsmrs[load_name]
+
+        if haskey(enrg_cnsmr_data, "EnergyConsumer.customerCount")
+            load_data["priority"] = enrg_cnsmr_data["EnergyConsumer.customerCount"]
+        end
     end
 end
 
 function ravens2math_add_bus_passthrough_default!(data_math::Dict{String,<:Any}, data_ravens::Dict{String,<:Any})
-    for (name, data) in get(data_math, "bus", Dict{Any,Dict{String,Any}}())
-        data["microgrid_id"] = "1"
+    # TODO: where to get the microgrid_id from RAVENS-JSON?
+    for (name, bus_data) in get(data_math, "bus", Dict{Any,Dict{String,Any}}())
+        bus_name = bus_data["name"]
+        conn_nodes = data_ravens["ConnectivityNode"]
+
+        if haskey(conn_nodes, bus_name)
+            conn_node_data = conn_nodes[bus_name]
+            if haskey(conn_node_data, "ConnectivityNode.microgridId")
+                bus_data["microgrid_id"] = conn_node_data["ConnectivityNode.microgridId"]
+            end
+        end
     end
 end
 
 function ravens2math_add_generator_passthrough_default!(data_math::Dict{String,<:Any}, data_ravens::Dict{String,<:Any})
-    for (name, data) in get(data_math, "gen", Dict{Any,Dict{String,Any}}())
-        data["inverter"] = 1
-        data["gen_model"] = 1
+    # TODO: where to get from RAVENS-JSON?
+    for (name, gen_data) in get(data_math, "gen", Dict{Any,Dict{String,Any}}())
+        gen_name = gen_data["name"]
+        rotng_machns = data_ravens["PowerSystemResource"]["Equipment"]["ConductingEquipment"]["EnergyConnection"]["RegulatingCondEq"]["RotatingMachine"]
+        if haskey(rotng_machns, gen_name)
+            gen_data = rotng_machns[gen_name]
+            if haskey(gen_data, "RotatingMachine.RotatingMachineResponse")
+                gen_data["gen_model"] = gen_data["RotatingMachine.RotatingMachineResponse"]
+            end
+            if haskey(gen_data, "RotatingMachine.Inverter")
+                gen_data["inverter"] = gen_data["RotatingMachine.Inverter"] # GRID_FOLLOWING, GRID_FORMING
+            end
+        end
     end
 end
 
 
 function ravens2math_add_storage_passthrough_default!(data_math::Dict{String,<:Any}, data_ravens::Dict{String,<:Any})
-    for (name, data) in get(data_math, "storage", Dict{Any,Dict{String,Any}}())
-        data["inverter"] = 1
-        data["gen_model"] = 1
-        data["phase_unbalance_ub"] = 1
+    # TODO: where to get from RAVENS-JSON?
+    for (name, storage_data) in get(data_math, "storage", Dict{Any,Dict{String,Any}}())
+        storage_name = storage_data["name"]
+        storages = data_ravens["PowerSystemResource"]["Equipment"]["ConductingEquipment"]["EnergyConnection"]["RegulatingCondEq"]["PowerElectronicsConnection"]
+        if haskey(storages, storage_name)
+            storage_data = storages[storage_name]
+            if haskey(storage_data, "PowerElectronicsConnection.PowerElectronicsUnit")
+                storage_data["gen_model"] = get(storage_data, "BatteryUnit.BatteryResponse", 1)
+            end
+            if haskey(storage_data, "PowerElectronicsConnection.Inverter")
+                storage_data["inverter"] = storage_data["PowerElectronicsConnection.Inverter"] # GRID_FOLLOWING, GRID_FORMING
+            end
+            if haskey(storage_data, "PowerElectronicsConnection.PhaseUnbalanceLimit")
+                storage_data["phase_unbalance_ub"] = storage_data["PowerElectronicsConnection.PhaseUnbalanceLimit"]
+            end
+        end
     end
 end
 
 
 function ravens2math_add_switch_passthrough_default!(data_math::Dict{String,<:Any}, data_ravens::Dict{String,<:Any})
-    for (name, data) in get(data_math, "switch", Dict{Any,Dict{String,Any}}())
-        data["vm_delta_pu_ub"] = 1
-        data["va_delta_deg_ub"] = 1
+    # TODO: where to get from RAVENS-JSON?
+    for (name, switch_data) in get(data_math, "switch", Dict{Any,Dict{String,Any}}())
+        switch_name = switch_data["name"]
+        switches = data_ravens["PowerSystemResource"]["Equipment"]["ConductingEquipment"]["Switch"]
+        if haskey(switches, switch_name)
+            switch_data = switches[switch_name]
+            if haskey(switch_data, "Switch.VoltageMagnitudeUpperBound")
+                switch_data["vm_delta_pu_ub"] = switch_data["Switch.VoltageMagnitudeUpperBound"]
+            end
+            if haskey(switch_data, "Switch.VoltageAngleUpperBound")
+                switch_data["va_delta_deg_ub"] = switch_data["Switch.VoltageAngleUpperBound"]
+            end
+        end
     end
 end
 
