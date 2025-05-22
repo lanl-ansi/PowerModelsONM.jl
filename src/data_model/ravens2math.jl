ce6b5ff2d1544e12837f57de647eeca96e4d32c9
jjospina <jjospina@lanl.gov>
2025-05-22 11:39:40 -0600

    REF: identation of ravens2math.jl and remove virtual buses from math2eng conversion.

--- src/data_model/ravens2math.jl
+++ src/data_model/ravens2math.jl
@@ -1,4 +1,4 @@
-function transform_data_model_ravens(ravens::T; multinetwork::Bool=false, global_keys::Set{String}=Set{String}(), ravens2math_extensions::Vector{<:Function}=Function[_ravens2math_passthrough_default_funcs!...], ravens2math_passthrough::Dict{String,<:Vector{<:String}}=Dict{String,Vector{String}}(), kwargs...)::T where T <: Dict{String,Any}
+function transform_data_model_ravens(ravens::T; multinetwork::Bool=false, global_keys::Set{String}=Set{String}(), ravens2math_extensions::Vector{<:Function}=Function[_ravens2math_passthrough_default_funcs!...], ravens2math_passthrough::Dict{String,<:Vector{<:String}}=Dict{String,Vector{String}}(), kwargs...)::T where T<:Dict{String,Any}
     PMD.transform_data_model_ravens(
         ravens;
         multinetwork=multinetwork,
@@ -47,20 +47,20 @@ function ravens2math_add_bus_passthrough_default!(data_math::Dict{String,<:Any},
 end
 
 function ravens2math_add_generator_passthrough_default!(data_math::Dict{String,<:Any}, data_ravens::Dict{String,<:Any})
-	# TODO: where to get from RAVENS-JSON?
-	for (_, gen_data) in get(data_math, "gen", Dict{Any,Dict{String,Any}}())
-		gen_name = gen_data["name"]
-		pecs = get(data_ravens["PowerSystemResource"]["Equipment"]["ConductingEquipment"]["EnergyConnection"]["RegulatingCondEq"], "PowerElectronicsConnection", Dict())
-		if haskey(pecs, gen_name)
-			pec_data = pecs[gen_name]
-			if haskey(pec_data, "PowerElectronicsConnection.PowerElectronicsConnectionResponse")
-				gen_data["gen_model"] = pec_data["PowerElectronicsConnection.PowerElectronicsConnectionResponse"]
-			end
-			if haskey(pec_data, "PowerElectronicsConnection.PowerElectronicsOperatingMode")
-				gen_data["inverter"] = get(pec_data["PowerElectronicsConnection.PowerElectronicsOperatingMode"], "PowerElectronicsOperatingMode.mode", "OperatingModeKind.gridFollowing") ==  "OperatingModeKind.gridForming" ? GRID_FORMING : GRID_FOLLOWING
-			end
-		end
-	end
+    # TODO: where to get from RAVENS-JSON?
+    for (_, gen_data) in get(data_math, "gen", Dict{Any,Dict{String,Any}}())
+        gen_name = gen_data["name"]
+        pecs = get(data_ravens["PowerSystemResource"]["Equipment"]["ConductingEquipment"]["EnergyConnection"]["RegulatingCondEq"], "PowerElectronicsConnection", Dict())
+        if haskey(pecs, gen_name)
+            pec_data = pecs[gen_name]
+            if haskey(pec_data, "PowerElectronicsConnection.PowerElectronicsConnectionResponse")
+                gen_data["gen_model"] = pec_data["PowerElectronicsConnection.PowerElectronicsConnectionResponse"]
+            end
+            if haskey(pec_data, "PowerElectronicsConnection.PowerElectronicsOperatingMode")
+                gen_data["inverter"] = get(pec_data["PowerElectronicsConnection.PowerElectronicsOperatingMode"], "PowerElectronicsOperatingMode.mode", "OperatingModeKind.gridFollowing") == "OperatingModeKind.gridForming" ? GRID_FORMING : GRID_FOLLOWING
+            end
+        end
+    end
 end
 
 
@@ -68,14 +68,14 @@ function ravens2math_add_storage_passthrough_default!(data_math::Dict{String,<:A
     # TODO: where to get from RAVENS-JSON?
     for (_, storage_data) in get(data_math, "storage", Dict{Any,Dict{String,Any}}())
         storage_name = storage_data["name"]
-				pecs = get(data_ravens["PowerSystemResource"]["Equipment"]["ConductingEquipment"]["EnergyConnection"]["RegulatingCondEq"], "PowerElectronicsConnection", Dict())
+        pecs = get(data_ravens["PowerSystemResource"]["Equipment"]["ConductingEquipment"]["EnergyConnection"]["RegulatingCondEq"], "PowerElectronicsConnection", Dict())
         if haskey(pecs, storage_name)
             pec_data = pecs[storage_name]
             if haskey(pec_data, "PowerElectronicsConnection.PowerElectronicsUnit")
                 storage_data["gen_model"] = get(pec_data, "BatteryUnit.BatteryResponse", 1)
             end
             if haskey(gen_data, "PowerElectronicsConnection.PowerElectronicsOperatingMode")
-                storage_data["inverter"] = get(pec_data["PowerElectronicsConnection.PowerElectronicsOperatingMode"], "PowerElectronicsOperatingMode.mode", "OperatingModeKind.gridFollowing") ==  "OperatingModeKind.gridForming" ? GRID_FORMING : GRID_FOLLOWING
+                storage_data["inverter"] = get(pec_data["PowerElectronicsConnection.PowerElectronicsOperatingMode"], "PowerElectronicsOperatingMode.mode", "OperatingModeKind.gridFollowing") == "OperatingModeKind.gridForming" ? GRID_FORMING : GRID_FOLLOWING
             end
             if haskey(pec_data, "PowerElectronicsConnection.PhaseUnbalanceLimit")
                 storage_data["phase_unbalance_ub"] = pec_data["PowerElectronicsConnection.PhaseUnbalanceLimit"]
@@ -89,7 +89,7 @@ function ravens2math_add_switch_passthrough_default!(data_math::Dict{String,<:An
     # TODO: where to get from RAVENS-JSON?
     for (name, switch_data) in get(data_math, "switch", Dict{Any,Dict{String,Any}}())
         switch_name = switch_data["name"]
-        switches = get(data_ravens["PowerSystemResource"]["Equipment"]["ConductingEquipment"],"Switch", Dict())
+        switches = get(data_ravens["PowerSystemResource"]["Equipment"]["ConductingEquipment"], "Switch", Dict())
         if haskey(switches, switch_name)
             switch_info = switches[switch_name]
             if haskey(switch_info, "Switch.VoltageMagnitudeUpperBound")
@@ -107,14 +107,14 @@ end
 
 function create_eng_nw_from_math(math_nw)
     new = Dict{String,Any}("nw" => Dict{String,Any}(),
-    "mn_lookup" => Dict{String,Float64}())
+        "mn_lookup" => Dict{String,Float64}())
 
     for (n, nw) in get(math_nw, "nw", Dict())
         new["nw"][n] = create_eng_from_math(nw, math_nw["bus_lookup"][n])
         if parse(Int, n) == 1
             new["mn_lookup"][n] = 0.0
         else
-            new["mn_lookup"][n] = (parse(Int, n)-1) * math_nw["nw"]["$(parse(Int, n)-1)"]["time_elapsed"]
+            new["mn_lookup"][n] = (parse(Int, n) - 1) * math_nw["nw"]["$(parse(Int, n)-1)"]["time_elapsed"]
         end
     end
 
@@ -129,141 +129,143 @@ end
 
 
 function create_eng_from_math(math, bus_lookup=missing)
-	new = Dict{String,Any}()
+    new = Dict{String,Any}()
 
-	if ismissing(bus_lookup)
-			bus_lookup = math["bus_lookup"]
-	end
+    if ismissing(bus_lookup)
+        bus_lookup = math["bus_lookup"]
+    end
 
-	bus_map = Dict{Int,String}(v => k for (k, v) in bus_lookup)
+    bus_map = Dict{Int,String}(v => k for (k, v) in bus_lookup)
 
-	# init settings dict
-	new["settings"] = deepcopy(math["settings"])
+    # init settings dict
+    new["settings"] = deepcopy(math["settings"])
 
-	for settings_key in ["vbases_buses", "vbases_default", "vbases_network"]
-		new["settings"][settings_key] = Dict{String, Float64}()
-		for (bus, val) in get(math["settings"], settings_key, Dict())
-			for (math_bus, eng_bus) in bus_map
-				if (string(math_bus) == bus)
-					new["settings"][settings_key]["$(eng_bus)"] = get(math["settings"][settings_key], string(math_bus), Inf)
-				end
-			end
-		end
-	end
+    for settings_key in ["vbases_buses", "vbases_default", "vbases_network"]
+        new["settings"][settings_key] = Dict{String,Float64}()
+        for (bus, val) in get(math["settings"], settings_key, Dict())
+            for (math_bus, eng_bus) in bus_map
+                if (string(math_bus) == bus)
+                    new["settings"][settings_key]["$(eng_bus)"] = get(math["settings"][settings_key], string(math_bus), Inf)
+                end
+            end
+        end
+    end
 
-	# TODO: correct vbases_default
-	new["settings"]["vbases_default"] = new["settings"]["vbases_buses"]
+    # TODO: correct vbases_default
+    new["settings"]["vbases_default"] = new["settings"]["vbases_buses"]
 
 
-	new["bus"] = Dict{String,Any}()
-	for (i, bus) in get(math, "bus", Dict())
-			new["bus"]["$(bus["name"])"] = Dict{String,Any}(
-					"terminals" => bus["terminals"],
-					"status" => bus["bus_type"] != 4 ? ENABLED : DISABLED,
-          "grounded" => bus["grounded"],
-					"vbase" => bus["vbase"],
-			)
-	end
+    new["bus"] = Dict{String,Any}()
+    for (i, bus) in get(math, "bus", Dict())
+        if !startswith(bus["name"], "_virtual")
+            new["bus"]["$(bus["name"])"] = Dict{String,Any}(
+                "terminals" => bus["terminals"],
+                "status" => bus["bus_type"] != 4 ? ENABLED : DISABLED,
+                "grounded" => bus["grounded"],
+                "vbase" => bus["vbase"],
+            )
+        end
+    end
 
-	new["line"] = Dict{String,Any}()
-	for (i, br) in get(math, "branch", Dict())
-			if !startswith(br["name"], "_virtual")
-					new["line"]["$(br["name"])"] = Dict{String,Any}(
-							"f_bus" => "$(bus_map[br["f_bus"]])",
-							"t_bus" => "$(bus_map[br["t_bus"]])",
-							"f_connections" => br["f_connections"],
-							"t_connections" => br["t_connections"],
-							"status" => Status(br["br_status"])
-					)
-			end
-	end
+    new["line"] = Dict{String,Any}()
+    for (i, br) in get(math, "branch", Dict())
+        if !startswith(br["name"], "_virtual")
+            new["line"]["$(br["name"])"] = Dict{String,Any}(
+                "f_bus" => "$(bus_map[br["f_bus"]])",
+                "t_bus" => "$(bus_map[br["t_bus"]])",
+                "f_connections" => br["f_connections"],
+                "t_connections" => br["t_connections"],
+                "status" => Status(br["br_status"])
+            )
+        end
+    end
 
-	new["switch"] = Dict{String,Any}()
-	for (i, sw) in get(math, "switch", Dict())
-			t_bus = sw["t_bus"]
-			if t_bus ∉ keys(bus_map)
-					for (i,br) in get(math, "branch", Dict())
-							if t_bus == br["f_bus"]
-									t_bus = br["t_bus"]
-									break
-							end
-					end
-			end
+    new["switch"] = Dict{String,Any}()
+    for (i, sw) in get(math, "switch", Dict())
+        t_bus = sw["t_bus"]
+        if t_bus ∉ keys(bus_map)
+            for (i, br) in get(math, "branch", Dict())
+                if t_bus == br["f_bus"]
+                    t_bus = br["t_bus"]
+                    break
+                end
+            end
+        end
 
-			new["switch"][sw["name"]] = Dict{String,Any}(
-					"f_bus" => bus_map[sw["f_bus"]],
-					"t_bus" => bus_map[t_bus],
-					"f_connections" => sw["f_connections"],
-					"t_connections" => sw["t_connections"],
-					"state" => SwitchState(sw["state"]),
-					"dispatchable" => Dispatchable(sw["dispatchable"]),
-					"status" => Status(sw["status"])
-			)
-	end
+        new["switch"][sw["name"]] = Dict{String,Any}(
+            "f_bus" => bus_map[sw["f_bus"]],
+            "t_bus" => bus_map[t_bus],
+            "f_connections" => sw["f_connections"],
+            "t_connections" => sw["t_connections"],
+            "state" => SwitchState(sw["state"]),
+            "dispatchable" => Dispatchable(sw["dispatchable"]),
+            "status" => Status(sw["status"])
+        )
+    end
 
-	new["load"] = Dict{String,Any}()
-	for (i, load) in get(math, "load", Dict())
-			new["load"]["$(load["name"])"] = Dict{String,Any}(
-					"bus" => bus_map[load["load_bus"]],
-					"connections" => load["connections"],
-					"configuration" => load["configuration"],
-					"model" => load["model"],
-					"dispatchable" => Dispatchable(load["dispatchable"]),
-					"pd_nom" => load["pd"],
-					"qd_nom" => load["qd"],
-					"status" => Status(load["status"])
-			)
-	end
+    new["load"] = Dict{String,Any}()
+    for (i, load) in get(math, "load", Dict())
+        new["load"]["$(load["name"])"] = Dict{String,Any}(
+            "bus" => bus_map[load["load_bus"]],
+            "connections" => load["connections"],
+            "configuration" => load["configuration"],
+            "model" => load["model"],
+            "dispatchable" => Dispatchable(load["dispatchable"]),
+            "pd_nom" => load["pd"],
+            "qd_nom" => load["qd"],
+            "status" => Status(load["status"])
+        )
+    end
 
-	new["generator"] = Dict{String,Any}()
-	new["solar"] = Dict{String,Any}()
-	new["voltage_source"] = Dict{String,Any}()
-	for (i, gen) in get(math, "gen", Dict())
-			bus_id = gen["gen_bus"]
-			if bus_id ∉ keys(bus_map)
-					for (i,br) in get(math, "branch", Dict())
-							if bus_id == br["f_bus"]
-									bus_id = br["t_bus"]
-									break
-							end
-					end
-			end
+    new["generator"] = Dict{String,Any}()
+    new["solar"] = Dict{String,Any}()
+    new["voltage_source"] = Dict{String,Any}()
+    for (i, gen) in get(math, "gen", Dict())
+        bus_id = gen["gen_bus"]
+        if bus_id ∉ keys(bus_map)
+            for (i, br) in get(math, "branch", Dict())
+                if bus_id == br["f_bus"]
+                    bus_id = br["t_bus"]
+                    break
+                end
+            end
+        end
 
-			data = Dict{String,Any}(
-					"bus" => bus_map[bus_id],
-					"connections" => gen["connections"],
-					"configuration" => gen["configuration"],
-					"pg_ub" => gen["pmax"],
-					"qg_ub" => gen["qmax"],
-					"inverter" => get(gen, "inverter", GRID_FOLLOWING),
-					"status" => Status(gen["gen_status"])
-			)
+        data = Dict{String,Any}(
+            "bus" => bus_map[bus_id],
+            "connections" => gen["connections"],
+            "configuration" => gen["configuration"],
+            "pg_ub" => gen["pmax"],
+            "qg_ub" => gen["qmax"],
+            "inverter" => get(gen, "inverter", GRID_FOLLOWING),
+            "status" => Status(gen["gen_status"])
+        )
 
-			if startswith(gen["source_id"], "generator") || startswith(gen["source_id"], "rotating_machine")
-					data["inverter"] = GRID_FORMING # TODO: assumption
-					new["generator"][split(gen["source_id"], "."; limit=2)[end]] = data
-			elseif startswith(gen["source_id"], "solar") || startswith(gen["source_id"], "photovoltaic_unit")
-					new["solar"][split(gen["source_id"], "."; limit=2)[end]] = data
-			elseif startswith(gen["source_id"], "voltage_source") || startswith(gen["source_id"], "energy_source")
-					data["inverter"] = GRID_FORMING
-					new["voltage_source"][split(gen["source_id"], "."; limit=2)[end]] = data
-			end
-	end
+        if startswith(gen["source_id"], "generator") || startswith(gen["source_id"], "rotating_machine")
+            data["inverter"] = GRID_FORMING # TODO: assumption
+            new["generator"][split(gen["source_id"], "."; limit=2)[end]] = data
+        elseif startswith(gen["source_id"], "solar") || startswith(gen["source_id"], "photovoltaic_unit")
+            new["solar"][split(gen["source_id"], "."; limit=2)[end]] = data
+        elseif startswith(gen["source_id"], "voltage_source") || startswith(gen["source_id"], "energy_source")
+            data["inverter"] = GRID_FORMING
+            new["voltage_source"][split(gen["source_id"], "."; limit=2)[end]] = data
+        end
+    end
 
-	new["storage"] = Dict{String,Any}()
-	for (i, strg) in get(math, "storage", Dict())
-			new["storage"][strg["name"]] = Dict{String,Any}(
-					"bus" => bus_map[strg["storage_bus"]],
-					"connections" => strg["connections"],
-					"configuration" => strg["configuration"],
-					"status" => Status(strg["status"]),
-					"energy" => strg["energy"],
-					"energy_ub" => strg["energy_rating"],
-					"inverter" => GRID_FORMING # TODO: hack
-			)
-	end
+    new["storage"] = Dict{String,Any}()
+    for (i, strg) in get(math, "storage", Dict())
+        new["storage"][strg["name"]] = Dict{String,Any}(
+            "bus" => bus_map[strg["storage_bus"]],
+            "connections" => strg["connections"],
+            "configuration" => strg["configuration"],
+            "status" => Status(strg["status"]),
+            "energy" => strg["energy"],
+            "energy_ub" => strg["energy_rating"],
+            "inverter" => GRID_FORMING # TODO: hack
+        )
+    end
 
-	return new
+    return new
 end
 
 
